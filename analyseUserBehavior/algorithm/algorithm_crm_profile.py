e8249068761238b4d41e0ab0968d53b39800cda2
flybetter <flybetter@163.com>
2019-03-06 16:23:11 +0800

    the first stage finished

--- analyseUserBehavior/algorithm/algorithm_crm_profile.py
+++ analyseUserBehavior/algorithm/algorithm_crm_profile.py
@@ -3,6 +3,24 @@ import multiprocessing as mp
 from datetime import datetime
 
 
+def operator_status(func):
+    """
+
+    :param func: decorator
+    :return:
+    """
+
+    def get_status(*args, **kwargs):
+        global func_result
+        try:
+            func_result = func(*args, **kwargs)
+        except Exception as e:
+            traceback.print_exc()
+        return func_result
+
+    return get_status
+
+
 class CrmProfile:
     def __init__(self, redis_host=REDIS_HOST, redis_crm_db=REDIS_CRM_DB, redis_db=REDIS_DB):
         crm_pool = redis.ConnectionPool(host=redis_host, db=redis_crm_db)
@@ -21,15 +39,11 @@ class CrmProfile:
             if re_data:
                 phone = re_data.group(1)
                 result = self.get_crm_profile_detail(phone, df)
-                temp_result=self.get_crm_house_data(phone, result)
-                if len(temp_result)>0:
-                    self.crm_profile_dict[phone]=temp_result
-                # self.crm_profile_dict.extend(temp_result)
-        self.redis_pipline_save()
+                result = self.get_crm_house_data(phone, result)
+                if len(result) > 0:
+                    self.crm_profile_dict[REDIS_CRM_PREFIX + phone] = result
 
-    def subprocess_fun(self, phone, df):
-        result = self.get_crm_profile_detail(phone, df)
-        self.get_crm_house_data(phone, result)
+        self.redis_pipline_save()
 
     def get_crm_house_data(self, phone, result):
         data = list()
@@ -39,14 +53,17 @@ class CrmProfile:
                 for json_data in json_datas:
                     data.extend(json.loads(json_data.decode('utf-8')))
         if len(data) > 0:
-            result = self.crm_profile_action(data, result)
-            return result
+            result_json = json.dumps(data, ensure_ascii=False)
+            df = pd.read_json(result_json, orient='records', dtype={'PRJ_ITEMNAME': np.str})
+            cities = dict()
+            for key, value in df.groupby('CITY_NAME'):
+                cities[key] = self.crm_profile_action(df, result)
+            return cities
         else:
             return data
 
-    def crm_profile_action(self, data, result):
-        result_json = json.dumps(data, ensure_ascii=False)
-        df = pd.read_json(result_json, orient='records')
+    @operator_status
+    def crm_profile_action(self, df, result):
         fiter_df = df[df['PRICE_SHOW'].astype(str).str.contains('元/㎡', na=False)]
         result['avg_price'] = fiter_df['PRICE_AVG'].mean()
         result['area'] = df['PIC_AREA'].mean()
@@ -60,7 +77,10 @@ class CrmProfile:
         result['bedroom'] = df['PIC_TYPE'].mean()
         result['kitchen'] = df['PIC_CHU'].mean()
         if len(df) > 0:
-            result['top_item_name'] = df['PRJ_ITEMNAME'].value_counts().index[0]
+            df = df.sort_values(by='START_TIME', ascending=False)
+            result['top_item_name'] = df['PRJ_ITEMNAME'].value_counts().index.get_values()[0]
+        else:
+            result['top_item_name'] = np.NAN
         return result
 
     def get_crm_profile_data(self):
@@ -83,22 +103,25 @@ class CrmProfile:
         result = dict()
         df_result = df[df['PHONE'].astype(str).str.contains(phone)]
         if len(df_result) > 0:
-            result['userId'] = df_result['ID']
-            result['idCard'] = df_result['IDCARD']
+            result['userId'] = df_result.iloc[0]['ID'].astype(str)
+            result['IDCard'] = df_result.iloc[0]['IDCARD']
+        else:
+            result['userId'] = np.NAN
+            result['IDCard'] = np.NAN
         return result
 
     def redis_save(self, phone, result):
-
         print('phone:{0},value:{1}'.format(phone, json.dumps(result, ensure_ascii=False)))
         self.crm_r.set(REDIS_CRM_PREFIX + phone, json.dumps(result, ensure_ascii=False))
 
     def redis_pipline_save(self):
-        # TODO mset
-        # pool = self.crm_r.pipeline()
-        # pool.mset(self.crm_profile_dict)
-        # pool.execute()
-        for i in self.crm_profile_dict:
-            print(i)
+        with self.crm_r.pipeline(transaction=False) as pipe:
+            for (k, v) in self.crm_profile_dict.items():
+                pipe.hmset(k, v)
+            pipe.execute()
+
+        # for (k, v) in self.crm_profile_dict.items():
+        #     print(k, json.dumps(v, ensure_ascii=False))
 
 
 def begin():
@@ -107,9 +130,9 @@ def begin():
 
 
 if __name__ == '__main__':
-    start_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    print("start time :" + start_time)
+    start_time = datetime.now()
+    print("start time :" + start_time.strftime('%Y-%m-%d %H:%M:%S'))
     crm_profile = CrmProfile()
     crm_profile.begin()
-    end_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    print("end time:" + end_time + " cost time:" + (end_time - start_time).seconds)
+    end_time = datetime.now()
+    print("end time:" + end_time.strftime('%Y-%m-%d %H:%M:%S') + " cost time:" + str((end_time - start_time).seconds))
